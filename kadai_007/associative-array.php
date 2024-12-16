d9287a379e44a8b6649fa952c0689a906eb9fed3
H.K <139024871+nanayo277@users.noreply.github.com>
2024-12-16 16:04:22 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+         print_r($personal_data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
